f15477b8ada9b9f2dfb9ba4379e84dbce2423a7b
UR Rehman <umair.rehman221@gmail.com>
2026-08-14 13:36:48 +0400

    Add comments for orchestrator flow in index.js
    Add orchestrator comments explaining the flow.

---
+++ index.js
@@ -0,0 +1,17 @@
+// This becomes the orchestrator.
+
+// It connects everything:
+
+// User clicks
+//     ↓
+// index.js event handler
+//     ↓
+// Todo/Project logic
+//     ↓
+// state changes
+//     ↓
+// storage.save()
+//     ↓
+// dom.render()
+
+
