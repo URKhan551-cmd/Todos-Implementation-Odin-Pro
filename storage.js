5e1033d204bcefbc30a67cf9ecf5ae12e4ad29ed
UR Rehman <umair.rehman221@gmail.com>
2026-08-15 18:21:52 +0400

    Combine saveData and loadData into saveNLoadData
    Refactor saveData and loadData into saveNLoadData function.

--- storage.js
+++ storage.js
@@ -1,2 +1,21 @@
-saveData()
-loadData()
+function saveNLoadData(){
+const TODO_STORAGE_KEY = "todos";
+  // saveData take todos[]   will get put into localStorage.
+function saveData(todos){
+   localStorage.setItem(TODO_STORAGE_KEY, JSON.stringify(todos));
+  return true;
+}
+function loadData(){
+  const storedTodos = localStorage.getItem(TODO_STORAGE_KEY);
+  if(!storedTodos){
+    return [];
+  }
+  
+  return JSON.parse(storedTodos);
+};
+
+  function clearTodos(){
+    localStorage.removeItem(TODO_STORAGE_KEY);
+  }
+  return {loadData, saveData, clearTodos}
+}
