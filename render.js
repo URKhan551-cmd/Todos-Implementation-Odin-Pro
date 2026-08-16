934c5674c4f0aab3245fe8eb71ede6e88ed08db9
UR Rehman <umair.rehman221@gmail.com>
2026-08-16 14:02:56 +0400

    Export renderTodos function from render.js
    Export renderTodos function for use in other modules.

--- render.js
+++ render.js
@@ -1,7 +1,9 @@
 
 
 renderProjects()
-renderTodos()
+export function renderTodos(todos){  // todos comes from storage.js and pass through todoManager.getTodos();
+   
+}
 renderTodoDetails()
 
 clearTodoForm() 
